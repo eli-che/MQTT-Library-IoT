d4f728a0ed1c9bfcd246e4daf50c8ada7679e7ec
Elias <44979236+eli-che@users.noreply.github.com>
2024-06-07 20:26:06 +0200

    Client connect handling

--- MqttClient.cs
+++ MqttClient.cs
@@ -15,12 +15,27 @@ namespace MQTT_Publisher
 
         public void Connect(string host, int port)
         {
+            _client = new TcpClient(host, port);
+            _stream = _client.GetStream();
 
+            var connectPacket = new MqttConnectPacket
+            {
+                clientId = "TestClient",
+                cleanSession = true,
+                keepAlive = 60
+            };
+            
+            var packetBytes = connectPacket.ToByteArray();
+            _stream.Write(packetBytes, 0, packetBytes.Length);
+
+            var response = new byte[4];
+            _stream.Read(response, 0, response.Length);
         }
 
         public void Disconnect()
         {
-
+            _stream.Close();
+            _client.Close();
         }
     }
 
